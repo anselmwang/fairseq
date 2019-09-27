bf5e7d4b342f15a9df1b04b2399ca39c56299d86
Yu Wang <yuwan@microsoft.com>
2019-09-27 16:04:28 -0700

    facilitate run

---
+++ docker_init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /workspace/dl_prog/fairseq
+
+pip install regex
+pip install --editable .
+
+pip install regex
+
+/bin/bash
---
+++ run_docker.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -v /home/yuwan/GitRoot/dl_prog:/workspace/dl_prog -it pytorch/pytorch /workspace/dl_prog/fairseq/docker_init.sh
